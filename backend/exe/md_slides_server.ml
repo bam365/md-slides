7a07dca5c9c5438df1b9fcfe5e3166db3e7a3f80
Blake Mitchell <bmitchel@nerdery.com>
2018-11-18 08:10:46 -0600

    organize handlers better

--- backend/exe/md_slides_server.ml
+++ backend/exe/md_slides_server.ml
@@ -1,3 +1,4 @@
+open CCFun
 open Mdslides
 open Mdslides_models
 open Opium.Std
@@ -6,21 +7,6 @@ let slides_filename = "slides.md"
 let config_file = "slides.yaml"
 
 let fe_path_env = "MD_SLIDES_FE_PATH"
-
-module FEPath = struct
-    let get () =
-        Sys.getenv_opt fe_path_env
-        |> CCOpt.get_or ~default:"fe"
-
-    let path s fe_base = fe_base ^ "/" ^ s
-
-    let index_file  = path "src/index.html"
-let build = path "build"
-
-    let css = path "src/build"
-end
-
-module Let_syntax = Mdslides.MResult.Let
 
 let load_config_file conf_file = 
     CCIO.with_in conf_file begin fun channel ->
@@ -31,51 +17,64 @@ let load_config_file conf_file =
             Presentation.from_toml toml
         | `Error(str, _) -> CCResult.Error(str)
     end
-        
-let serve_index fe_base _ = 
-    CCIO.with_in (FEPath.index_file fe_base) begin fun channel ->
-        let contents = CCIO.read_all channel in
-        `Html contents |> respond'
-    end
 
-let main fe_base = get "/" (serve_index fe_base)
+module Frontend = struct
+    let get_base_path () =
+        Sys.getenv_opt fe_path_env
+        |> CCOpt.get_or ~default:"fe"
 
-let slide fe_base = get "/slide/:slidenum" (serve_index fe_base)
-
-let build fe_base = Middleware.static ~local_path:(FEPath.build fe_base) ~uri_prefix:"/build"
-
-let css fe_base = Middleware.static ~local_path:(FEPath.css fe_base) ~uri_prefix:"/css"
-
-let json_response s =
-    `Json (Ezjsonm.from_string s) |> respond' 
-    
-
-let api_slides = get "/api/slides" begin fun _ -> 
-    let  slides = Types.(
-        { slides = SlideFile.load_slides slides_filename }
-    ) in
-    Json.string_of_slides slides
-    |> json_response
+    let handlers fe_base = 
+        let path s = fe_base ^ "/" ^ s in
+        let serve_index _ = 
+            CCIO.with_in (path "src/index.html") begin fun channel ->
+                let contents = CCIO.read_all channel in
+                `Html contents |> respond'
+            end
+        in
+        let build = 
+            Middleware.static ~local_path:(path "build") ~uri_prefix:"/build" 
+        in
+        let css = 
+            Middleware.static ~local_path:(path "src/css") ~uri_prefix:"/css"
+        in
+        middleware build 
+        %> middleware css 
+        %> get "/" serve_index
+        %> get "/slide/:slidenum" serve_index 
 end
 
-let api_presentation_config conf = get "/api/presentation-config" begin fun _ ->
-    Json.string_of_presentation_config conf
-    |> json_response
+module Api = struct
+    let json_response s =
+        `Json (Ezjsonm.from_string s) |> respond' 
+    
+
+    let slides = get "/api/slides" begin fun _ -> 
+        let  slides = Types.(
+            { slides = SlideFile.load_slides slides_filename }
+        ) in
+        Json.string_of_slides slides
+        |> json_response
+    end
+
+    let presentation_config conf = get "/api/presentation-config" begin fun _ ->
+        Json.string_of_presentation_config conf
+        |> json_response
+    end
+
+    let handlers conf =
+        slides
+        %> presentation_config conf
 end
 
 let run_server config fe_base = 
     App.empty
-    |> api_slides
-    |> api_presentation_config config
-    |> middleware (build fe_base)
-    |> middleware (css fe_base)
-    |> slide fe_base
-    |> main fe_base
+    |> Api.handlers config
+    |> Frontend.handlers fe_base
     |> App.run_command
     |> ignore
 
 let run () = 
-    let fe_base = FEPath.get () in
+    let fe_base = Frontend.get_base_path () in
     match load_config_file "md-slides.toml" with
     | Error(str) -> print_endline ("Could not load config: " ^ str)
     | Ok(conf) -> run_server conf fe_base
